8c9c1d880b2291ac1e908f9a4735df2cf0e049b9
Jordan Thoms <jords@shadowservices.biz>
2011-02-15 21:07:51 +1300

    log when started from alarm.

--- src/biz/shadowservices/DegreesToolbox/UpdateReciever.java
+++ src/biz/shadowservices/DegreesToolbox/UpdateReciever.java
@@ -3,12 +3,14 @@ package biz.shadowservices.DegreesToolbox;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class UpdateReciever extends BroadcastReceiver {
-
+	public static String TAG = "2DegreesUpdateReciever";
 	@Override
 	public void onReceive(Context context, Intent intent) {
         // To prevent any ANR timeouts, we perform the update in a service
+		Log.d(TAG, "Started from alarm");
         context.startService(new Intent(context, UpdateWidgetService.class));
 	}
 
