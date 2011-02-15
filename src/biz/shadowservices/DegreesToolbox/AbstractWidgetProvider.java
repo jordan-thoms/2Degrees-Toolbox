54582f34e7bc98668afc235a697f0aad80c6e054
Jordan Thoms <jords@shadowservices.biz>
2011-02-15 19:10:26 +1300

    read the preference for update frequencey and use it

--- src/biz/shadowservices/DegreesToolbox/AbstractWidgetProvider.java
+++ src/biz/shadowservices/DegreesToolbox/AbstractWidgetProvider.java
@@ -9,6 +9,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public abstract class AbstractWidgetProvider extends AppWidgetProvider {
@@ -20,14 +22,33 @@ public abstract class AbstractWidgetProvider extends AppWidgetProvider {
         context.startService(new Intent(context, UpdateWidgetService.class));
     }
     public void onEnabled(Context context) {
+    	setAlarm(context);
+    }
+    static public void setAlarm(Context context) {
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+		long interval;
+		String stringInterval = sp.getString("updateFreq", "half_day");
+		if (stringInterval.equals("hour")) {
+			Log.d(TAG, "Hourly updates");
+			interval = AlarmManager.INTERVAL_HOUR;
+		} else if (stringInterval.equals("half_day")) {
+			Log.d(TAG, "Half day updates");
+			interval = AlarmManager.INTERVAL_HALF_DAY;
+		} else if (stringInterval.equals("day")){
+			Log.d(TAG, "daily updates");
+			interval = AlarmManager.INTERVAL_DAY;
+		} else {
+			Log.d(TAG, "other interval??? - using half day");
+			interval = AlarmManager.INTERVAL_HALF_DAY;
+		}
     	Intent intent = new Intent("biz.shadowservices.DegreesToolbox.WIDGET_UPDATE");
     	PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
     	AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
     	Calendar calendar = Calendar.getInstance();
     	calendar.setTimeInMillis(System.currentTimeMillis());
-    	calendar.add(Calendar.HOUR, 1);
+    	calendar.add(Calendar.MINUTE, 10);
     	Log.d(TAG, "Setting alarm");
-    	alarmManager.setRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), AlarmManager.INTERVAL_HALF_DAY, pendingIntent);
+    	alarmManager.setInexactRepeating(AlarmManager.RTC, calendar.getTimeInMillis(), interval, pendingIntent);
     }
     public void onDisabled(Context context) {
     	// We need to check if there are any widgets of other sizes still on the screen.
