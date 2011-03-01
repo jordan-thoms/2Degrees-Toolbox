afec090946d09e48bf819ed5a2c85a4f7ee9b888
Jordan Thoms <jords@shadowservices.biz>
2011-03-01 20:34:18 +1300

    changes to update system - cleaner handling of multiple providers, loading pre-update

--- src/biz/shadowservices/DegreesToolbox/AbstractWidgetUpdater.java
+++ src/biz/shadowservices/DegreesToolbox/AbstractWidgetUpdater.java
@@ -2,6 +2,8 @@ package biz.shadowservices.DegreesToolbox;
 
 import java.util.Date;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -34,6 +36,35 @@ public abstract class AbstractWidgetUpdater {
 			 Log.d(TAG, e.getMessage());
 			 return "";
 		}
+    }
+    protected abstract ComponentName getComponentName(Context context);
+    public void updateWidgets(Context context, boolean force, int error) {
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        ComponentName provider = getComponentName(context);
+	    int [] widgetIds = manager.getAppWidgetIds(provider);
+	    for (int widget : widgetIds) {
+	        RemoteViews updateViews = this.buildUpdate(context, widget, force, error);
+		     // Push update to home screen
+		    Log.d(TAG, "Pushing updates for 2x1 widget");
+		    manager.updateAppWidget(widget, updateViews);
+		}
+
+    }
+    RemoteViews buildLoadingUpdate(Context context) {
+    	Log.d(TAG, "Building updates");
+    	RemoteViews updateViews = new RemoteViews(context.getPackageName(), getLayoutId());
+    	fillRemoteViewsLoading(updateViews, context);
+        return updateViews;
+    }
+    protected abstract void  fillRemoteViewsLoading(RemoteViews updateViews, Context context);
+    public void widgetLoading(Context context) {
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        ComponentName provider = getComponentName(context);
+        RemoteViews updateViews = this.buildLoadingUpdate(context);
+		// Push update to home screen
+		Log.d(TAG, "Pushing updates for 2x1 widget");
+		manager.updateAppWidget(provider, updateViews);
+
     }
 
 }
