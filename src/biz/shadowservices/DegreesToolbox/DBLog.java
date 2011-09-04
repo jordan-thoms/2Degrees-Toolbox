0fa6ab5a49747862e034aee89c3aa68fb96e1f57
ords <jords@shadowservices.biz>
2011-09-04 20:23:13 +1200

    Code to manage size of log

--- src/biz/shadowservices/DegreesToolbox/DBLog.java
+++ src/biz/shadowservices/DegreesToolbox/DBLog.java
@@ -4,7 +4,9 @@ import java.util.Date;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public  class DBLog {
 	public static void insertMessage(Context c, String severity, String tag, String message) {
@@ -17,6 +19,17 @@ public  class DBLog {
 		values.put("tag", tag);
 		values.put("message", message);
 		db.insert("log", null, values);
+		if((int) (Math.random()*200) == 5) {
+			Cursor results = db.rawQuery("SELECT COUNT(1) FROM log", null);
+			results.moveToFirst();
+			int noLines = results.getInt(0);
+			results.close();
+			if (noLines > 50) {
+				String sql = "DELETE FROM log WHERE `id` IN (SELECT `id` FROM log ORDER BY `id` ASC LIMIT " + (noLines-50) +  ")";
+				Log.d("2DegreesDBLog", "Deleting " + (noLines-50) + "rows. Sql: " +sql );
+				db.execSQL(sql);
+			}
+		}
 		db.close();
 	}
 }
