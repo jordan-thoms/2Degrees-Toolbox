c302d3dd2cff8a7f06edf864a5d136ad6d486572
ords <jords@shadowservices.biz>
2011-09-27 20:41:45 +1300

    Remove old debugging output in Util.

--- src/biz/shadowservices/DegreesToolbox/Util.java
+++ src/biz/shadowservices/DegreesToolbox/Util.java
@@ -25,19 +25,12 @@ public class Util {
 	// Couple of utility functions
 	public static int dpToPx(Context context, float dp) {
 		final float scale = context.getResources().getDisplayMetrics().density;
-		Log.d("2DegreesSCALE", Float.toString(scale));
 		int size = (int) (dp * scale + 0.5f);
-		Log.d("2DegreesDp", Float.toString(dp));
-		Log.d("2DegreesSize", Integer.toString(size));
 		return size;
 	}
 	public static int pxToSp(Context context, float px) {
 		final float scale = context.getResources().getDisplayMetrics().scaledDensity;
-		Log.d("2DegreesSCALESP", Float.toString(scale));
 		int size = (int) (px * scale + 0.5f);
-		Log.d("2DegreesPx", Float.toString(px));
-		Log.d("2DegreesSizeSp", Integer.toString(size));
-
 		return  size;
 	}
 	public static int dpToSp(Context context, int dp) {
