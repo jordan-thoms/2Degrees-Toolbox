fb63ccd85f5bc1b3183fb616fa057aa167c617c0
Jordan Thoms <jordan@thoms.net.nz>
2012-11-26 10:27:35 +1300

    Fix updates with new Included text

--- Toolbox/src/biz/shadowservices/DegreesToolbox/DataFetcher.java
+++ Toolbox/src/biz/shadowservices/DegreesToolbox/DataFetcher.java
@@ -306,7 +306,7 @@ public class DataFetcher {
 						String[] amountParts = amountHTML.split("&nbsp;", 2);
 						//Log.d(TAG, amountParts[0]);
 						//Log.d(TAG, amountParts[1]);
-						if (amountParts[0].equals("Included") ||  amountParts[0].equals("All You Need")) {
+						if (amountParts[0].contains("Included") ||  amountParts[0].equals("All You Need")) {
 							value = Values.INCLUDED;
 						} else {
 							try {
